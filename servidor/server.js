6405315e05136fca0ea7fd63515cd34eccaca9be
GleenPeltroche <gleenpeltroche@gmail.com>
2022-03-18 16:46:11 -0500

    chat server

--- servidor/server.js
+++ servidor/server.js
@@ -1,20 +1,16 @@
-//Servidor con express
 const express = require("express");
 const http = require("http");
 const app = express();
 const servidor = http.createServer(app);
 
-//Inicializamos socketio
 const socketio = require("socket.io");
 const io = socketio(servidor);
 
-//Funcionalidad de socket.io en el servidor
 io.on("connection", (socket) => {
   let nombre;
 
   socket.on("conectado", (nomb) => {
     nombre = nomb;
-    //socket.broadcast.emit manda el mensaje a todos los clientes excepto al que ha enviado el mensaje
     socket.broadcast.emit("mensajes", {
       nombre: nombre,
       mensaje: `${nombre} ha entrado en la sala del chat`,
@@ -22,7 +18,6 @@ io.on("connection", (socket) => {
   });
 
   socket.on("mensaje", (nombre, mensaje) => {
-    //io.emit manda el mensaje a todos los clientes conectados al chat
     io.emit("mensajes", { nombre, mensaje });
   });
 
